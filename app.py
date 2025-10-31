809939ae18ccdb42f548df2ce7cb4b04ac014278
Nerdydatacool <swati.ro435@gmail.com>
2025-10-31 13:51:50 -0400

    add github actions ci/cd workflow

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+
+def home():
+    return "Hello from Flask CI/CD via Docker on port 8080!"
+
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=8080)
+
+
